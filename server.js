7ea8c0577af1e2f26c2cb0656bb96fe9f3822c7b
Felix <46941065+felix911104@users.noreply.github.com>
2019-07-29 00:03:39 -0700

    test4

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-// Dependencies
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
 
-// Requiring Note and Article models
+
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
 
-// Scraping tools
+
 var request = require("request");
 var cheerio = require("cheerio");
 var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/mongoHeadlines"
 mongoose.connect(MONGODB_URL);
-// Set mongoose to leverage built in JavaScript ES6 Promises
+
 mongoose.Promise = Promise;
 
-//Define port
+
 var port = process.env.PORT || 3000
 
-// Initialize Express
+
 var app = express();
 
-// Use morgan and body parser with our app
+
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-// Make public a static dir
+
 app.use(express.static("public"));
 
-// Set Handlebars.
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({
@@ -41,12 +41,12 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
-// Database configuration with mongoose
+
 mongoose.connect("mongodb://user1:password1@ds113650.mlab.com:13650/heroku_t4wfzf9v");
-//mongoose.connect("mongodb://localhost/mongoscraper");
+
 var db = mongoose.connection;
 
-// Show any mongoose errors
+
 db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
 });
